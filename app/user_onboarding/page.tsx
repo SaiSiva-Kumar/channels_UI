dcd6c5d2d01457fc763d9738bf93c8a999297927
sivakumar0906 <saisivakumar0906@gmail.com>
2025-07-09 12:23:24 +0530

    issue solved

--- app/user_onboarding/page.tsx
+++ app/user_onboarding/page.tsx
@@ -38,12 +38,15 @@ export default function OnboardingPage() {
         const data = await res.json()
         localStorage.setItem('channels_data', JSON.stringify(data.joined_channels))
         localStorage.setItem('created_channels_data', JSON.stringify(data.created_channels))
+        localStorage.setItem('joined_channels_last_id', data.joined_channels.last_id ? data.joined_channels.last_id.toString() : '')
+        localStorage.setItem('created_channels_last_id', data.created_channels.last_id ? data.created_channels.last_id.toString() : '')
       } else {
-        localStorage.setItem('channels_data', JSON.stringify([]))
-        localStorage.setItem('created_channels_data', JSON.stringify([]))
+        localStorage.setItem('channels_data', JSON.stringify({ channels: [] }))
+        localStorage.setItem('created_channels_data', JSON.stringify({ channels: [] }))
+        localStorage.setItem('joined_channels_last_id', '')
+        localStorage.setItem('created_channels_last_id', '')
       }
 
-      console.log(user)
       router.push('/')
     } catch (error) {
       console.error('❌ Error signing in:', error)
